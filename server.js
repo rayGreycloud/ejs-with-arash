9609519653ef83698a0e61c826122d77e75158ae
rayGreycloud <rayGreycloud@gmail.com>
2016-10-05 13:29:05 -0700

    ejs demo with hello world

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
+var ejs = require('ejs');
+var engine = require('ejs-mate');
 
 var app = express();
 
+app.engine('ejs', engine);
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', function(req, res, err) {
+  res.render('home');
+});
 
 app.listen(3000, function(err) {
   if (err) {
